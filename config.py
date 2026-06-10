54f5d79410181692e5f4f1aef0f13bece5253db8
vishal vishwakarma <119778329+vishal343012@users.noreply.github.com>
2026-06-11 00:21:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://root:root@localhost/my_database'
+        'postgresql://apppppwedd-server:Password123##@localhost/apppppwedd-database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
